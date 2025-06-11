ea9a89f13bb2e97af3eab3306d7526d34cc58435
jja42 <jja42w@gmail.com>
2025-06-11 19:39:15 -0400

    Running Python Files

--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
 # from subdirectory.filename import function_name
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python_files import run_python_file
+
+### FILE INFO
 
 #print(get_files_info("calculator", "."))
 
@@ -10,10 +14,34 @@ from functions.get_file_content import get_file_content
 
 #print(get_files_info("calculator", "../"))
 
+
+### FILE CONTENT
+
 #print(get_file_content("calculator", "lorem.txt"))
 
-print(get_file_content("calculator", "main.py"))
+#print(get_file_content("calculator", "main.py"))
 
-print(get_file_content("calculator", "pkg/calculator.py"))
+#print(get_file_content("calculator", "pkg/calculator.py"))
 
-print(get_file_content("calculator", "/bin/cat"))
+#print(get_file_content("calculator", "/bin/cat"))
+
+
+### WRITE FILES
+ 
+#print(write_file("calculator", "lorems.txt", "wait, this isn't lorem ipsum"))
+
+#print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+
+#print(write_file("calculator", "notreal/text.txt", "this folder and file didn't exist earlier"))
+
+#print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+
+#### RUN FILES
+print(run_python_file("calculator", "main.py"))
+
+print(run_python_file("calculator", "tests.py"))
+
+print(run_python_file("calculator", "../main.py"))
+
+print(run_python_file("calculator", "nonexistent.py"))
